b2d59ad63272bb38e4200a3a202672e74206b53e
Lucas Vieira <web.lucasvieira@gmail.com>
2020-05-05 01:38:33 -0300

    listando transações e balanço

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,8 +8,8 @@ interface Balance {
 
 interface CreateTransactionDTO {
   title: string;
-  value: number;
   type: 'income' | 'outcome';
+  value: number;
 }
 
 class TransactionsRepository {
@@ -23,9 +23,34 @@ class TransactionsRepository {
     return this.transactions;
   }
 
-  // public getBalance(): Balance {
-  //   // TODO
-  // }
+  public getBalance(): Balance {
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
+  }
 
   public create({ title, type, value }: CreateTransactionDTO): Transaction {
     const transaction = new Transaction({ title, type, value });
