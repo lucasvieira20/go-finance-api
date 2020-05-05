1659a48fcfff09743bc8ef9615b4fda7f31bce14
Lucas Vieira <web.lucasvieira@gmail.com>
2020-05-05 01:45:20 -0300

    Verifico o saldo antes do outcome

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -14,6 +14,16 @@ class CreateTransactionService {
   }
 
   public execute({ title, type, value }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enought value');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       type,
